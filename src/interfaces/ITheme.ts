0dd5886bc46d6752d9ecbf7073388f67d5918a6f
Amirhossein <amirzenoozi72@gmail.com>
2021-05-30 15:41:14 +0430

    :zap: Update Typographies Interface

--- src/interfaces/ITheme.ts
+++ src/interfaces/ITheme.ts
@@ -1,5 +1,5 @@
 import { IPallete } from "./IPalette";
-import React from "react";
+import { Typographies } from 'src/interfaces/typographies';
 
 export interface ITheme {
   palette: IPallete;
@@ -12,15 +12,5 @@ export interface ITheme {
     xlg: string;
     xxlg: string;
   };
-  typography: {
-    body1: React.CSSProperties;
-    body2: React.CSSProperties;
-    caption: React.CSSProperties;
-    h1: React.CSSProperties;
-    h2: React.CSSProperties;
-    h3: React.CSSProperties;
-    h4: React.CSSProperties;
-    h5: React.CSSProperties;
-    h6: React.CSSProperties;
-  };
+  typography: Typographies;
 }
---
+++ src/interfaces/typographies.ts
@@ -0,0 +1,10 @@
+export interface Typographies {
+  head1: number;
+  head2: number;
+  title1: number;
+  title2: number;
+  body1: number;
+  body2: number;
+  cap1: number;
+  cap2: number;
+}
