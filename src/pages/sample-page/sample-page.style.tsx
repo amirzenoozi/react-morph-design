e9847b4c65e99d7eebfa8fa0e6585fcfe0e61fcd
Amirhossein <amirzenoozi72@gmail.com>
2021-05-30 13:07:18 +0430

    :tada: Add SamplePage For Home Route

---
+++ src/pages/sample-page/sample-page.style.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const PageContainer = styled.nav`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  color: #FFFFFF;
+  background-color: #2A2C2E;
+`;
+
+export { PageContainer };
---
+++ src/pages/sample-page/sample-page.tsx
@@ -0,0 +1,13 @@
+import React  from 'react';
+import { PageContainer } from './sample-page.style';
+
+const SamplePage = () => {
+
+  return (
+    <PageContainer>
+      Hello World!
+    </PageContainer>
+  );
+};
+
+export default SamplePage;
