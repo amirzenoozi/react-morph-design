fe0c1f7a5efb3354ab9be1bd45d7829d770f1e03
Amirhossein <amirzenoozi72@gmail.com>
2021-05-30 15:41:34 +0430

    :rotating_light: Fix Linter Issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,34 @@
 module.exports =  {
-    root: true,
-    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
-    plugins: [
-        '@typescript-eslint',
-        'react'
-    ],
-    extends:  [
-        'eslint:recommended',  // Uses the recommended rules from @eslint-plugin-react
-    ],
-    env: {
-        "browser": true,
-        "jest": true,
-        "node": true,
+  root: true,
+  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  plugins: [
+    '@typescript-eslint',
+    'react'
+  ],
+  extends:  [
+    'eslint:recommended',  // Uses the recommended rules from @eslint-plugin-react
+  ],
+  env: {
+    "browser": true,
+    "jest": true,
+    "node": true,
+  },
+  rules:  {
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "indent": ["error", 2],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "no-undef": ["error", {
+      "typeof": true,
+    }]
+  },
+  globals: {
+    JSX: true,
+  },
+  settings:  {
+    react:  {
+      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
     },
-    rules:  {
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "indent": ["error", 2],
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": ["warn"],
-        "no-undef": ["error", {
-            "typeof": true,
-        }]
-    },
-    settings:  {
-        react:  {
-            version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
-    },
+  },
 };
